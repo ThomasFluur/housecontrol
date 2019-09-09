bc8cf76c4e26a99087212731306602cda1415171
Thomas Fluur <ThomasFluur@users.noreply.github.com>
2019-09-09 13:45:41 +0200

    Flytta ut beskrvingen

---
+++ Brukare/Brukare.mm
@@ -0,0 +1,87 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1476961131614" ID="ID_932869743" LINK="../Hus%20kontroll.mm" MODIFIED="1567774056202" TEXT="Brukare">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Byt till brukare f&#246;r att slippa prickar,
+    </p>
+    <p>
+      Skriv anv&#228;ndnigsfall user case och user story.
+    </p>
+    <p>
+      Val av&#160;&#160;redan existerande l&#246;sningar styr ber&#228;ttelsen
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1476961159505" ID="ID_248268093" MODIFIED="1567774179747" POSITION="right" TEXT="Mont&#xf6;r">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Andra&#160;&#160;system tillf&#228;lliga ( Skulle&#160;&#160;kunna vara hantverkare etc..)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1476961185500" MODIFIED="1567773072408" POSITION="right" TEXT="Installat&#xf6;r">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Montera system troligen tekninik intresserad &#228;gare. Om produkt s&#229;&#160;&#160;blir det en annan&#160;&#160;niv&#229;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1476961207420" MODIFIED="1567773340837" POSITION="right" TEXT="Anv&#xe4;ndare">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Daglig dags funktioner. Flera niv&#229;er
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1476961230283" MODIFIED="1567773651321" POSITION="right" TEXT="G&#xe4;ster">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Skulle&#160;&#160;kunna&#160;&#160;vara hyresg&#228;st. Daglig funktion med&#160;&#160;begr&#228;nsat funktion. Tillf&#228;llig har fast panel eller mobil enhet knuten
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1476962891297" ID="ID_803781433" MODIFIED="1567774108634" POSITION="right" TEXT="&#xc4;gare">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Statistik&#160;&#160;statusar. hyresg&#228;ster och anv&#228;ndare
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
