daf8f5ec3acc30a22a43e3469ed539302ea11170
Thomas Fluur <ThomasFluur@users.noreply.github.com>
2019-09-09 13:44:38 +0200

    Update plan

---
+++ UPS/UPS.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1476369908870" ID="ID_1086667726" LINK="../Hus%20kontroll.mm" MODIFIED="1567767262644" TEXT="UPS">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Finns f&#228;rdiga att k&#246;pa men de &#228;r t&#228;nkta f&#246;r desktop datorer. Placeringar mot router kan&#160;&#160;bli sv&#229;r
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1476961259844" MODIFIED="1477466653148" POSITION="left" TEXT="Styr och kontroll enheter (Datorer)">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Om gamla laptop datorer anv&#228;nds kolla hur byte till batteri mod sker
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1476964133974" ID="ID_234224603" MODIFIED="1476964151820" TEXT="BIOS">
+<node CREATED="1476964155858" ID="ID_407421806" MODIFIED="1476964246813" TEXT="Wake up on LAN"/>
+<node CREATED="1567771431194" ID="ID_852487038" MODIFIED="1567771463991" TEXT="Last state"/>
+</node>
+<node CREATED="1476964263017" ID="ID_1044057410" MODIFIED="1476964288902" TEXT="Schemalagd &#xe5;terstart"/>
+</node>
+<node CREATED="1476961319966" ID="ID_1752395877" LINK="https://www.dustinhome.se/product/5010587805/3s-550va300w-ups?ssel=false&amp;_ga=2.67206097.1153223007.1567766545-19335252.1567766545" MODIFIED="1567767407899" POSITION="left" TEXT="Kommunikations enhet">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Finns det f&#228;rdiga att k&#246;pa? Drar lite str&#246;m s&#229; extern UPS kan f&#229; det sv&#229;rt?
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
